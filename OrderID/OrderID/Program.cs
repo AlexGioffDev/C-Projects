ad86270c1ab933a151413fb3c9f1d2126ab8b079
AlexGioffDev <alex.gioffre_dev@outlook.it>
2024-09-17 15:49:45 +0200

    C# Project 2

---
+++ OrderID/OrderID/Program.cs
@@ -0,0 +1,12 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] idsArray = orderStream.Split(",");
+Array.Sort(idsArray);
+foreach (var id in idsArray)
+{
+  if(id.Length == 4){
+    System.Console.WriteLine(id);
+  } else {
+    System.Console.WriteLine($"{id}\t\t- Error");
+  }
+}
