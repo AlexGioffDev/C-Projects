23adc68a27f344f05fe01a94db376eb99915e2e6
AlexGioffDev <alex.gioffre_dev@outlook.it>
2024-09-18 08:15:20 +0200

    C# Project 4

---
+++ EmailWorker/EmailWorker/Program.cs
@@ -0,0 +1,26 @@
+﻿string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+void ShowEmail(string[,] employeers, string domain = "contoso.com") {
+  for(int i = 0; i < employeers.GetLength(0); i++) {
+    string email = employeers[i, 0].Substring(0, 2).ToLower() + employeers[i, 1].ToLower() + "@" + domain;
+    System.Console.WriteLine(email);
+  }
+}
+
+ShowEmail(corporate);
+ShowEmail(external, externalDomain);
+
+
